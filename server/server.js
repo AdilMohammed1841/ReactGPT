bf3a7eab6d5af0b708eab585c8e95a993b900c81
adil mohammed <adilmohammed1841@gmail.com>
2023-05-27 19:54:43 +0530

    api key setup

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Configuration , OpenAIApi } = require('openai');
 const cors = require('cors');
+require('dotenv').config()
 
 const port = process.env.PORT || 5000;
 
 const config = new Configuration({
-    organization: "org-0wBMtfVBserw5Vx2RBTgrPPc",
-    apiKey: "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"
+    organization: process.env.ORG_ID,
+    apiKey: process.env.API_KEY,
 })
 const app = express();  
 
@@ -15,9 +16,9 @@ const openai = new OpenAIApi(config)
 
 app.use(bodyParser.json());
 app.use(cors());
-
 app.post('/chat',async (req, res) => {
     try {
+        console.log(process.env.PORT,'asdasdasdasdasd')
         const { prompt } = req.body;
         const data = await openai.createChatCompletion({
             model: "gpt-3.5-turbo",
@@ -26,7 +27,6 @@ app.post('/chat',async (req, res) => {
         console.log(data.data.choices[0].message)
         res.send({data: data.data.choices[0].message })
     } catch (error) {
-        console.log(error,'asdasdasd')
         res.send({error: error})
     }
 });
